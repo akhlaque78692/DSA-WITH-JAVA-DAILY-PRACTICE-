f3ddbc6bf3c0b545cfb9355aacb8f15435cabb6a
Akhlaque Hussain <139735291+akhlaque78692@users.noreply.github.com>
2026-09-25 16:39:22 +0530

    Time: 5 ms (32.85%), Space: 45.7 MB (40.08%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,27 +1,39 @@
 class Solution {
 
-      Integer dp[][] ;
+      int dp[][] ;
 
-        int f(List<List<Integer>> t, int row, int col){
-                 if(row==t.size()-1){
+        // int f(List<List<Integer>> t, int row, int col){
+        //          if(row==t.size()-1){
                           
-                        return  t.get(row).get(col);
-                 }
-                 if(dp[row][col]!=null){
-                    return dp[row][col];
-                 }
+        //                 return  t.get(row).get(col);
+        //          }
+        //          if(dp[row][col]!=null){
+        //             return dp[row][col];
+        //          }
                  
-                int oneWay=  t.get(row).get(col)+f(t,row+1, col);
-                int second =  t.get(row).get(col)+f(t,row+1,col+1);
-                dp[row][col] = Math.min(oneWay , second);
-            return dp[row][col] ;
-        }
+        //         int oneWay=  t.get(row).get(col)+f(t,row+1, col);
+        //         int second =  t.get(row).get(col)+f(t,row+1,col+1);
+        //         dp[row][col] = Math.min(oneWay , second);
+        //     return dp[row][col] ;
+        // }
     public int minimumTotal(List<List<Integer>> t) {
-             dp=  new Integer[t.size()][t.size()];
-                  int row  =  0;
-                  int col= 0 ;
-                  return f(t , row, col);
+             dp=  new int[t.size()][t.size()];
+            //     //   int row  =  0;
+            //     //   int col= 0 ;
+            //     //   return f(t , row, col);
+              int  n  =  t.size();
+              for(int  i = 0  ; i < n ; i++){
+                    dp[n-1][i]=t.get(n-1).get(i);
+              }
 
+              for(int  row  = n-2 ; row>=0 ; row--){
+                   for(int col = 0 ;  col<=row ; col++){
+                        int l = t.get(row).get(col)+dp[row+1][col];
+                        int r = t.get(row).get(col)+dp[row+1][col+1];
+                        dp[row][col] = Math.min(l,r);
+                   }
+              }
+return dp[0][0];
  
     }
 }
